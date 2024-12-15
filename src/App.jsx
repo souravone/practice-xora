abce03c1f6f96d341c067a79e8d5204c9c0deb6b
Sourav Das <souravdas360@gmail.com>
2024-12-15 15:22:13 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
